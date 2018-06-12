37c33708e923c02ce18ffb8e5b9c26b33168f381
Eduardo da Silva <eduardo@mareiwa.com>
2018-06-11 23:25:12 -0600

    babel-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,44 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+
 const TARGET = process.env.npm_lifecycle_event;
+
 const PATHS = {
     app: path.join(__dirname, 'app'), build: path.join(__dirname, 'build')
 };
-
 
 const common = {
     entry: {
         app: PATHS.app
     },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
     output: {
         path: PATHS.build,
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader'],
+                include: PATHS.app
+            },
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel-loader'],
+                include: PATHS.app
+            }
+        ]
     }
 }
 
 if(TARGET === 'start' || !TARGET) {
     module.exports = merge(common, {
+        devtool: 'eval-source-map',
         devServer: {
             contentBase: PATHS.build,
             historyApiFallback: true,
@@ -30,10 +50,12 @@ if(TARGET === 'start' || !TARGET) {
             port: process.env.PORT || 3000
         },
             plugins: [
-            new webpack.HotModuleReplacementPlugin()
-        ]
-    });
+                new webpack.HotModuleReplacementPlugin()
+            ]
+        }
+    );
 }
+
 if(TARGET === 'build') {
     module.exports = merge(common, {});
 }
